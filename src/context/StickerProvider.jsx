5035d12a91979599d526776c9c83556bc311c0f6
mamolina01 <matiasnmolina1@gmail.com>
2023-06-14 18:26:33 -0300

    login and register with backend

--- src/context/StickerProvider.jsx
+++ src/context/StickerProvider.jsx
@@ -3,81 +3,102 @@ import { StickerContext } from "./StickerContext";
 import { generateID } from "../helpers/generateID";
 
 export const StickerProvider = ({ children }) => {
-	const [user, setUser] = useState(
-		JSON.parse(localStorage.getItem("user")) ?? {}
-	);
-	const [profile, setProfile] = useState(
-		JSON.parse(localStorage.getItem("profile")) ?? {}
-	);
-	const [isLogged, setIsLogged] = useState(
-		JSON.parse(localStorage.getItem("isLogged")) ?? false
-	);
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) ?? {}
+  );
+  const [profile, setProfile] = useState(
+    JSON.parse(localStorage.getItem("profile")) ?? {}
+  );
+  const [isLogged, setIsLogged] = useState(
+    JSON.parse(localStorage.getItem("isLogged")) ?? false
+  );
 
-	const [stickers, setStickers] = useState(
-		JSON.parse(localStorage.getItem("stickers")) ?? []
-	);
+  const [stickers, setStickers] = useState(
+    JSON.parse(localStorage.getItem("stickers")) ?? []
+  );
 
-	const setLogout = () => {
-		setUser({});
-		setProfile({});
-		setStickers([]);
-		setIsLogged(false);
-	};
+  const setLogout = () => {
+    setUser({});
+    setProfile({});
+    setStickers([]);
+    setIsLogged(false);
+  };
 
-	const addSticker = (newSticker) => {
-		newSticker.id = generateID();
-		setStickers([...stickers, newSticker]);
-	};
+  const addSticker = (newSticker) => {
+    newSticker.id = generateID();
+    setStickers([...stickers, newSticker]);
+  };
 
-	const removeSticker = (stickerToRemove) => {
-		let temp = stickers;
-		stickerToRemove.map(
-			(sticker) => (temp = temp.filter((item) => item.id !== sticker.id))
-		);
+  const removeSticker = (stickerToRemove) => {
+    let temp = stickers;
+    stickerToRemove.map(
+      (sticker) => (temp = temp.filter((item) => item.id !== sticker.id))
+    );
 
-		setStickers(temp);
-	};
+    setStickers(temp);
+  };
 
-	const editSticker = (stickerToEdit) => {
-		const tempStickers = stickers.map((item) =>
-			item.id === stickerToEdit.id ? stickerToEdit : item
-		);
-		setStickers(tempStickers);
-	};
+  const editSticker = (stickerToEdit) => {
+    const tempStickers = stickers.map((item) =>
+      item.id === stickerToEdit.id ? stickerToEdit : item
+    );
+    setStickers(tempStickers);
+  };
 
-	useEffect(() => {
-		localStorage.setItem("stickers", JSON.stringify(stickers));
-	}, [stickers]);
+  const checkAuthToken = async () => {
+    const token = localStorage.getItem("token");
+    if (!token) return setLogout();
 
-	useEffect(() => {
-		localStorage.setItem("user", JSON.stringify(user));
-	}, [user]);
+    try {
+      const data = await calendarApi.get("auth/renew");
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+    } catch (error) {
+      localStorage.clear();
+      setLogout();
+    }
+  };
 
-	useEffect(() => {
-		localStorage.setItem("profile", JSON.stringify(profile));
-	}, [profile]);
+  useEffect(() => {
+    checkAuthToken();
+  }, []);
 
-	useEffect(() => {
-		localStorage.setItem("isLogged", JSON.stringify(isLogged));
-	}, [isLogged]);
+  useEffect(() => {
+    localStorage.setItem("stickers", JSON.stringify(stickers));
+  }, [stickers]);
 
-	return (
-		<StickerContext.Provider
-			value={{
-				stickers,
-				user,
-				isLogged,
-				profile,
-				setProfile,
-				setIsLogged,
-				setUser,
-				addSticker,
-				removeSticker,
-				editSticker,
-				setLogout,
-			}}
-		>
-			{children}
-		</StickerContext.Provider>
-	);
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(user));
+  }, [user]);
+
+  useEffect(() => {
+    localStorage.setItem("profile", JSON.stringify(profile));
+  }, [profile]);
+
+  useEffect(() => {
+    localStorage.setItem("isLogged", JSON.stringify(isLogged));
+  }, [isLogged]);
+
+  return (
+    <StickerContext.Provider
+      value={{
+        //Properties
+        stickers,
+        user,
+        isLogged,
+        profile,
+
+        //Methods
+        setProfile,
+        setIsLogged,
+        setUser,
+        addSticker,
+        removeSticker,
+        editSticker,
+        setLogout,
+      }}
+    >
+      {children}
+    </StickerContext.Provider>
+  );
 };
--- src/hooks/useAuthenthicated.js
+++ src/hooks/useAuthenthicated.js
@@ -3,32 +3,95 @@ import { useEffect } from "react";
 import { StickerContext } from "../context";
 
 export const useAuthenthicated = () => {
-	const { setUser, setProfile, setIsLogged } = useContext(StickerContext);
+  const { setUser, setProfile, setIsLogged, setLogout } =
+    useContext(StickerContext);
 
-	const startLogin = async ({ email, password }) => {};
+  const startLogin = async ({ email, password }) => {
+    try {
+      const data = await fetch("http://localhost:4000/api/auth", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => response.json())
+        .then((json) => json);
 
-	const startRegister = async ({ name, email, password }) => {
-		try {
-			const { data } = await calendarApi.post(
-				"http://localhost:4000/api/auth/register",
-				{
-					name,
-					email,
-					password,
-				}
-			);
-			localStorage.setItem("token", data.token);
-			localStorage.setItem("token-init-date", new Date().getTime());
-			setUser({ email });
-			setProfile({ name });
-			setIsLogged(true);
-		} catch (error) {
-			return error.response.data?.msg || "--";
-		}
-	};
+      if (!data.ok) {
+        return data.msg;
+      }
 
-	return {
-		startLogin,
-		startRegister,
-	};
+      console.log(data);
+      //   localStorage.setItem("token", data.token);
+      //   localStorage.setItem("token-init-date", new Date().getTime());
+      //   setUser({ email });
+      //   setProfile({ name });
+      //   setIsLogged(true);
+      return false;
+    } catch (error) {
+      //   console.log(error);
+      //   console.log(error?.msg);
+      return error?.msg || "--";
+    }
+  };
+
+  const startRegister = async ({ name, email, password }) => {
+    try {
+      const data = await fetch("http://localhost:4000/api/auth/register", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      })
+        .then((response) => response.json())
+        .then((json) => json);
+
+      if (!data.ok) {
+        return data.msg;
+      }
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      setUser({ email });
+      setProfile({ name });
+      setIsLogged(true);
+      return false;
+    } catch (error) {
+      //   console.log(error);
+      //   console.log(error?.msg);
+      return error?.msg || "--";
+    }
+  };
+
+  const startSetProfile = async ({ logo, instagram, whatsapp }) => {
+    try {
+      const data = await fetch("http://localhost:4000/api/profile", {
+        method: "POST",
+        body: JSON.stringify({ name, email, password }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+          "x-token": "token",
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => json);
+
+      if (!data.ok) {
+        return data.msg;
+      }
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("token-init-date", new Date().getTime());
+      setUser({ email });
+      setProfile({ name });
+      setIsLogged(true);
+      return false;
+    } catch (error) {
+      //   console.log(error);
+      //   console.log(error?.msg);
+      return error?.msg || "--";
+    }
+  };
+
+  return {
+    startLogin,
+    startRegister,
+    startSetProfile,
+  };
 };
