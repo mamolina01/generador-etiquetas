0e02ff2b3cafe11ab85d23d6e5a2d9ba4795efd6
Matias Molina <matiasnmolina1@gmail.com>
2023-06-15 19:40:23 -0300

    renew token done

--- src/hooks/useManageStickers.js
+++ src/hooks/useManageStickers.js
@@ -5,45 +5,69 @@ import { generateID } from "../helpers";
 export const useManageStickers = () => {
 	const { addSticker, editSticker } = useContext(StickerContext);
 
-	const saveSticker = async (sticker) =>
-		// sticker
-		{
-			const token = localStorage.getItem("token") ?? "";
-			sticker.id = generateID();
-			try {
-				const data = await fetch("http://localhost:4000/api/stickers", {
-					method: "POST",
-					body: JSON.stringify(sticker),
-					headers: {
-						"Content-type": "application/json; charset=UTF-8",
-						"x-token": token,
-					},
-				})
-					.then((response) => response.json())
-					.then((json) => json);
+	const saveSticker = async (sticker) => {
+		const token = localStorage.getItem("token") ?? "";
+		try {
+			const response = await fetch("http://localhost:4000/api/stickers", {
+				method: "POST",
+				body: JSON.stringify(sticker),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+					"x-token": token,
+				},
+			})
+				.then((response) => response.json())
+				.then((json) => json);
 
-				console.log(data);
-				if (!data.ok) {
-					console.log(data);
-					return data.msg;
-				}
-
-				console.log(sticker);
-				addSticker(sticker);
-				return false;
-			} catch (error) {
-				console.log(error);
-				return error?.msg || "--";
+			console.log(response);
+			if (!response.ok) {
+				return response.msg;
 			}
-		};
-	const readStickers = async () => {};
+
+			console.log(sticker);
+			addSticker(sticker);
+			return false;
+		} catch (error) {
+			console.log(error);
+			return error?.msg || "--";
+		}
+	};
+
+	const getStickers = async () => {
+		const token = localStorage.getItem("token") ?? "";
+		try {
+			const data = await fetch("http://localhost:4000/api/stickers", {
+				method: "POST",
+				body: JSON.stringify(sticker),
+				headers: {
+					"Content-type": "application/json; charset=UTF-8",
+					"x-token": token,
+				},
+			})
+				.then((response) => response.json())
+				.then((json) => json);
+
+			console.log(data);
+			if (!data.ok) {
+				console.log(data);
+				return data.msg;
+			}
+
+			console.log(sticker);
+			addSticker(sticker);
+			return false;
+		} catch (error) {
+			console.log(error);
+			return error?.msg || "--";
+		}
+	};
 
 	const deleteSticker = async () => {};
 	const updateSticker = async () => {};
 
 	return {
 		saveSticker,
-		readStickers,
+		getStickers,
 		deleteSticker,
 		updateSticker,
 	};
--- src/router/MyRoutes.jsx
+++ src/router/MyRoutes.jsx
@@ -1,8 +1,8 @@
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
 } from "react-router-dom";
 // import { StickerGenerator, StickerList, StickerSettings } from "../pages";
 // import { SideBar } from "../components/SideBar";
@@ -10,20 +10,27 @@ import { useContext } from "react";
 import { StickerContext } from "../context";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { StickerRoutes } from "../pages/routes/StickerRoutes";
+import { useManageStickers } from "../hooks";
+import { useEffect } from "react";
 
 export const MyRoutes = () => {
-  const { isLogged } = useContext(StickerContext);
+	const { isLogged } = useContext(StickerContext);
+	const { getStickers } = useManageStickers();
 
-  return (
-    <Router>
-      <Routes>
-        {isLogged ? (
-          <Route exact path="/*" element={<StickerRoutes />} />
-        ) : (
-          <Route path="/auth/*" element={<AuthRoutes />} />
-        )}
-        <Route path="/*" element={<Navigate to="/auth/settings" />} />
-      </Routes>
-    </Router>
-  );
+	useEffect(() => {
+		getStickers();
+	}, []);
+
+	return (
+		<Router>
+			<Routes>
+				{isLogged ? (
+					<Route exact path="/*" element={<StickerRoutes />} />
+				) : (
+					<Route path="/auth/*" element={<AuthRoutes />} />
+				)}
+				<Route path="/*" element={<Navigate to="/auth/settings" />} />
+			</Routes>
+		</Router>
+	);
 };
